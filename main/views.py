98c3d46ee00aa0830ee2631178400d1ff0181234
Voznytsia <dmitryvoznitsa@gmail.com>
2022-10-09 18:06:27 +0300

    add user && category notations getting

--- main/views.py
+++ main/views.py
@@ -40,14 +40,14 @@ def get_categories():
     return jsonify({"categories": CATEGORIES})
 
 
-@app.route("/category", methods=['Post'])
+@app.route("/category", methods=['POST'])
 def create_category():
     request_data = {}
-    global categories_id
-    categories_id += 1
-    request_data["id"] = categories_id
     try:
         request_data["category_name"] = request.get_json()["category_name"]
+        global categories_id
+        categories_id += 1
+        request_data["id"] = categories_id
     except:
         return "Incorrect input"
 
@@ -60,14 +60,14 @@ def get_users():
     return jsonify({"users": USERS})
 
 
-@app.route("/user", methods=['Post'])
+@app.route("/user", methods=['POST'])
 def create_user():
     request_data = {}
-    global users_id
-    users_id += 1
-    request_data["id"] = users_id
     try:
         request_data["name"] = request.get_json()["name"]
+        global users_id
+        users_id += 1
+        request_data["id"] = users_id
     except:
         return "Incorrect input"
 
@@ -80,16 +80,16 @@ def get_notations():
     return jsonify({"notations": NOTATIONS})
 
 
-@app.route("/notation", methods=['Post'])
+@app.route("/notation", methods=['POST'])
 def create_notation():
     request_data = request.get_json()
-    global note_id
-    note_id += 1
     try:
         if not (validation("id", request.get_json()["user_id"], USERS) and validation("id",
                 request.get_json()["category_id"], CATEGORIES)):
             return "User or category not found"
-        request_data["id"] = note_id
+        global notations_id
+        notations_id += 1
+        request_data["id"] = notations_id
         request_data["date_of_creating"] = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
         request_data["price"] = request.get_json()["price"]
     except:
@@ -97,3 +97,32 @@ def create_notation():
 
     NOTATIONS.append(request_data)
     return request_data
+
+
+@app.route("/user-notations", methods=["POST"])
+def get_user_notations():
+    request_data = request.get_json()
+    try:
+        id_user = request_data["user_id"]
+        user_notations = []
+        for i in NOTATIONS:
+            if i["user_id"] == id_user:
+                user_notations.append(i)
+        return jsonify(user_notations)
+    except:
+        return "Error bad request"
+
+
+@app.route("/user-categories-notations", methods=["POST"])
+def get_user_categories_notations():
+    request_data = request.get_json()
+    try:
+        id_user = request_data["user_id"]
+        id_category = request_data["category_id"]
+        user_notations = []
+        for i in NOTATIONS:
+            if i["user_id"] == id_user and i["category_id"] == id_category:
+                user_notations.append(i)
+        return jsonify(user_notations)
+    except:
+        return "Error bad request"
